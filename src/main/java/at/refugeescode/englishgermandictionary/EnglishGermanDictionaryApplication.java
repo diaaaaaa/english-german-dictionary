93b6fcc6916f868c6ae9c4634494255dcc35768e
diaaaaaa <ite.diaa@gmail.com>
2018-03-24 17:01:36 +0100

    add spring

---
+++ src/main/java/at/refugeescode/englishgermandictionary/EnglishGermanDictionaryApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.englishgermandictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishGermanDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishGermanDictionaryApplication.class, args);
+	}
+}
